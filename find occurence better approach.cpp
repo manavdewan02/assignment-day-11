cd36aaf3901eb4ee5da01a6186c6e1c63ff54217
manavdewan02 <72863213+manavdewan02@users.noreply.github.com>
2022-05-02 16:12:53 +0530

    Update find occurence better approach.cpp

--- find occurence better approach.cpp
+++ find occurence better approach.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main(){
     int n=11;
+    int maxi=INT_MIN;
     int arr[n]={1,2,1,1,3,2,4,5,5,1,1};
-    int arr1[10]={0};
+    for(int i=0;i<n;i++){
+        maxi=max(maxi,arr[i]);
+    }
+    int arr1[maxi+1]={0};
     for(int i=0;i<n;i++){
         arr1[arr[i]]++;
     }
-    for(int i=0;i<10;i++){
+    for(int i=0;i<maxi+1;i++){
         if(arr1[i]>0){
             cout << i << " occurs: " << arr1[i] << " times" << endl;
         }
